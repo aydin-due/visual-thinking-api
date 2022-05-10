7cf5c18469a39f6c7a96e831be22888a6aed7512
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-05-10 14:56:45 -0700

    Reader + StudentController + unit tests

---
+++ test/controllers/StudentController.test.js
@@ -0,0 +1,19 @@
+const StudentController = require("../../lib/controllers/StudentController");
+
+describe("Tests para StudentController", () => {
+    test("1: Consultar todos los estudiantes con todos sus campos", () => {
+        const allStudents = StudentController.getStudents();
+        expect(allStudents.length).toBe(51);
+        expect(allStudents[allStudents.length-1].name).toBe("Clare");
+    }),
+    test("2. Consultar los emails de todos los estudiantes que tengan certificación", () => {
+        const emailsWithCertification = StudentController.getEmailsWithCertification();
+        expect(emailsWithCertification.length).toBe(29);
+        expect(emailsWithCertification[0].id).toBe("6264d5d89f1df827eb84bb23");
+    }),
+    test("3. Consultar todos los estudiantes que tengan credits mayor a 500", () => {
+        const studentsAbove500Credits = StudentController.getStudentsAbove500Credits()
+        expect(studentsAbove500Credits.length).toBe(27)
+        expect(studentsAbove500Credits[0].name).toBe("Warren")
+    });
+});
--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -1,19 +1,19 @@
 const StudentService = require("../../lib/services/StudentService");
 
 describe("Tests para StudentService", () => {
-    test("Requerimiento 1: Consultar todos los estudiantes con todos sus campos", () => {
+    test("1. Consultar todos los estudiantes con todos sus campos", () => {
         const students = [{id: "1", name:"student1"}, {id: "1", name:"student1"}];
         const allStudents = StudentService.getStudents(students)
         expect(allStudents.length).toBe(2);
         expect(allStudents[0].id).toBe("1");
     }),
-    test("Requerimiento 2: Consultar los emails de todos los estudiantes que tengan certificación", () => {
+    test("2. Consultar los emails de todos los estudiantes que tengan certificación", () => {
         const students = [{email:"student1@gmail.com", haveCertification:true}, {email:"student2@gmail.com", haveCertification:false}];
-        const emailsWithCeritifcation = StudentService.getEmailsWithCertification(students)
-        expect(emailsWithCeritifcation.length).toBe(1);
-        expect(emailsWithCeritifcation[0].email).toBe("student1@gmail.com");
+        const emailsWithCertification = StudentService.getEmailsWithCertification(students)
+        expect(emailsWithCertification.length).toBe(1);
+        expect(emailsWithCertification[0].email).toBe("student1@gmail.com");
     }),
-    test("Requerimiento 3: Consultar todos los estudiantes que tengan credits mayor a 500", () => {
+    test("3. Consultar todos los estudiantes que tengan credits mayor a 500", () => {
         const students = [{id:1, credits:199}, {id:2, credits:501}];
         const studentsAbove500Credits = StudentService.getStudentsAbove500Credits(students)
         expect(studentsAbove500Credits.length).toBe(1);
